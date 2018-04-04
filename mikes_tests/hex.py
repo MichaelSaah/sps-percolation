46caebdfc79f240d3a92d1671e28df04965dc8dd
Michael Saah <mike.saah@gmail.com>
2018-04-04 18:25:01 -0400

    reorganized tools and tests, wrote bond test and hexagonal test added images from tests

---
+++ mikes_tests/hex.py
@@ -0,0 +1,24 @@
+from tools.graphs import BondGraph, HexGraph
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import eigsh
+import numpy as np
+
+n = 100 # number of trials
+size = 20 # size of domain (size*size)
+p_range = np.linspace(0.05, 0.95, 30) # range of Bernoulli probabilities to test over
+full_perc_count = np.zeros((len(p_range), 1)) # count number of fully percolated graphs
+
+for i,p in enumerate(p_range):
+    for _ in range(n):
+        G = HexGraph(size,p)
+        if G.is_fully_percolated():
+            full_perc_count[i] += 1
+
+percent_perc = full_perc_count / n
+
+plt.plot(p_range, percent_perc, 'b.')
+plt.xlabel('probility of edge closure')
+plt.ylabel('porportion fully percolated')
+plt.title('Hexagonal percolation, ' + str(n) + ' samples, ' + str(size) + 'x' + str(size) + ' domain')
+plt.show()
---
+++ mikes_tests/tools/graphs.py
@@ -0,0 +1,167 @@
+import networkx as nx
+import numpy as np
+
+class BondGraph(nx.Graph):
+    
+    def __init__(self, size, p):
+        super().__init__()
+
+        self.size = size
+
+        # generate grid graph with random edges
+        self.add_nodes_from(self.gen_nodes(size))
+        self.add_edges_from(self.gen_edges(size, p))
+    
+        # find wet nodes (assume percolation from bottom)
+        wet_nodes = set()
+        for k in range(size):
+            wet_nodes = wet_nodes | nx.node_connected_component(self, (k,0)) # set union
+        self.wet_nodes = wet_nodes
+
+        # find dry nodes
+        self.dry_nodes = {node for node in self.nodes if node not in self.wet_nodes}
+
+
+    def is_fully_percolated(self):
+        end_nodes = {node for node in self.nodes if node[1] == self.size-1}
+        if len(self.wet_nodes.intersection(end_nodes)) == 0:
+            return False
+        else:
+            return True
+
+
+    def get_dry_lines(self):
+        edges = self.subgraph(self.dry_nodes).edges
+        
+        return self.edges_as_lines(edges)
+
+
+    def get_wet_lines(self):
+        edges = self.subgraph(self.wet_nodes).edges
+
+        return self.edges_as_lines(edges)
+
+
+    @staticmethod
+    def gen_nodes(n):
+        """
+        Generate nodes on the nxn integer grid
+        """
+    
+        for i in range(n):
+            for j in range(n):
+                yield (i,j)
+    
+
+    @staticmethod
+    def gen_edges(n,p):
+        """
+        Generate edges between nodes on the nxn integer grid with probability p
+        """
+    
+        # horizontal edges
+        for i in range(n):
+            for j in range(n-1):
+                if np.random.rand() < p:
+                    yield ((i, j), (i, j+1))
+    
+        # vertical edges
+        for j in range(n):
+            for i in range(n-1):
+                if np.random.rand() < p:
+                    yield ((i, j), (i+1, j))
+    
+
+    @staticmethod
+    def edges_as_lines(edges):
+        """
+        Helper fct for plotting edges    
+        """
+
+        return [((e[0][0], e[1][0]), (e[0][1], e[1][1])) for e in edges]
+
+
+
+class HexGraph(nx.Graph):
+
+    def __init__(self, size, p):
+        super().__init__()
+
+        self.size = size
+
+        # generate grid graph with random edges
+        self.add_nodes_from(self.gen_nodes(size))
+        self.add_edges_from(self.gen_edges(size, p))
+
+        # find wet nodes (assume percolation from bottom)
+        wet_nodes = set()
+        for k in range(size):
+            wet_nodes = wet_nodes | nx.node_connected_component(self, (k, 0))  # set union
+        self.wet_nodes = wet_nodes
+
+        # find dry nodes
+        self.dry_nodes = {node for node in self.nodes if node not in self.wet_nodes}
+
+
+    def is_fully_percolated(self):
+        end_nodes = {node for node in self.nodes if node[1] == self.size-1}
+        if len(self.wet_nodes.intersection(end_nodes)) == 0:
+            return False
+        else:
+            return True
+
+
+    def get_dry_lines(self):
+        edges = self.subgraph(self.dry_nodes).edges
+
+        return self.edges_as_lines(edges)
+
+
+    def get_wet_lines(self):
+        edges = self.subgraph(self.wet_nodes).edges
+
+        return self.edges_as_lines(edges)
+
+
+    @staticmethod
+    def gen_nodes(n):
+        """
+        Generate nodes on the nxn integer grid
+        """
+
+        for i in range(n):
+            for j in range(n):
+                yield (i, j)
+
+
+    @staticmethod
+    def gen_edges(n, p):
+        """
+        Generate edges between nodes on the nxn hexagonal grid with probability p
+        """
+
+        # horizontal edges
+        for i in range(n):
+            if i % 2 == 0:
+                for j in range(0, n - 1, 2):
+                    if np.random.rand() < p:
+                        yield ((i, j), (i, j + 1))
+            elif i % 2 == 1:
+                for j in range(1, n - 1, 2):
+                    if np.random.rand() < p:
+                        yield ((i, j), (i, j + 1))
+
+        # vertical edges
+        for j in range(n):
+            for i in range(n - 1):
+                if np.random.rand() < p:
+                    yield ((i, j), (i + 1, j))
+
+
+    @staticmethod
+    def edges_as_lines(edges):
+        """
+        Helper fct for plotting edges
+        """
+
+        return [((e[0][0], e[1][0]), (e[0][1], e[1][1])) for e in edges]
